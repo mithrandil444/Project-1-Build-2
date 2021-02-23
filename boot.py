ca76f8b61bc9aa150ec9f276cd41f793475bdb9d
mithrandil444 <72928954+mithrandil444@users.noreply.github.com>
2021-02-23 10:02:11 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -9,7 +9,7 @@ import time
 
 wlan = WLAN(mode=WLAN.STA)
 pycom.heartbeat(False)
-wlan.connect(ssid='telenet-C9C22', auth=(WLAN.WPA2, 'k0T3bU6R0EyA'))
+wlan.connect(ssid='', auth=(WLAN.WPA2, ''))
 
 # ---- Loop ----
 
