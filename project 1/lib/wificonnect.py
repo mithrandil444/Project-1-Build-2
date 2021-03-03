ef4f5364d12f81372b2e2b1b0c47fd5525383f6a
mithrandil444 <sven.devisscher@student.kdg.be>
2021-03-03 10:24:39 +0100

    add libraries

---
+++ project 1/lib/wificonnect.py
@@ -0,0 +1,23 @@
+# ---- Imports ----
+
+import pycom
+from network import WLAN
+import machine
+import time
+
+# ---- Config ----
+
+wlan = WLAN(mode=WLAN.STA)
+pycom.heartbeat(False)
+wlan.connect(ssid='IoT', auth=(WLAN.WPA2, 'KdGIoT92!'))
+
+# ---- Loop ----
+
+while not wlan.isconnected():
+    time.sleep(2)
+    pycom.rgbled(0xFF0000)          # Red
+    print("no connection")
+    machine.idle()
+print("WiFi connected succesfully")
+print(wlan.ifconfig())
+pycom.rgbled(0x00FF00)              # Green
