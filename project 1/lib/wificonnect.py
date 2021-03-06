81614f38ba6c01a94b6cc9ff87805dc6e6ead964
mithrandil444 <sven.devisscher@student.kdg.be>
2021-03-06 19:27:45 +0100

    add new code

--- project 1/lib/wificonnect.py
+++ project 1/lib/wificonnect.py
@@ -5,19 +5,20 @@ from network import WLAN
 import machine
 import time
 
-# ---- Config ----
 
-wlan = WLAN(mode=WLAN.STA)
-pycom.heartbeat(False)
-wlan.connect(ssid='IoT', auth=(WLAN.WPA2, 'KdGIoT92!'))
+def connect():
 
-# ---- Loop ----
+    wlan = WLAN(mode=WLAN.STA)
+    pycom.heartbeat(False)
+    wlan.connect(ssid='telenet-C9C22', auth=(WLAN.WPA2, 'k0T3bU6R0EyA'))
 
-while not wlan.isconnected():
-    time.sleep(2)
-    pycom.rgbled(0xFF0000)          # Red
-    print("no connection")
-    machine.idle()
-print("WiFi connected succesfully")
-print(wlan.ifconfig())
-pycom.rgbled(0x00FF00)              # Green
+
+
+    while not wlan.isconnected():
+        time.sleep(5)
+        pycom.rgbled(0xFF0000)          # Red
+        print("no connection")
+        machine.idle()
+    print("WiFi connected succesfully")
+    print(wlan.ifconfig())
+    pycom.rgbled(0x00FF00)              # Green
--- project 1/main.py
+++ project 1/main.py
@@ -1,2 +1,34 @@
 import sonarsensor
+import loraconnect2
+from time import sleep
 import wificonnect
+import urequests as requests
+
+# adafruit config
+aio_key = "aio_SWaA65ahbUNaL8Qd3D6o28GMq4Uy"
+username = "mithrandil444"
+feed_name = "houthakken"
+
+
+
+
+
+#loraconnect2.setup_lora()
+
+#distance= sonarsensor.send_distance()
+#loraconnect2.send_to_lora(distance)
+wificonnect.connect()
+
+while True:
+
+    distance= sonarsensor.send_distance()
+    url = 'https://io.adafruit.com/api/v2/' + username + '/feeds/' + feed_name + '/data'
+    body = {'value': distance}
+    headers = {'X-AIO-Key': aio_key, 'Content-Type': 'application/json'}
+    try:
+        r = requests.post(url, json=body, headers=headers)
+        print(r.text)
+        r.close()
+    except Exception as e:
+        print(e)
+    sleep(10)
